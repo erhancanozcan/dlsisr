Fixes MSE and VGG loss scaling bug where I was scaling by shape[1] which is channel dim = 1. Also removes adversarial loss scaling 1e-3 and sets recon loss lambda = 1. Fixed logging bug where losses were getting overwritten in losses_MSE.csv and data getting written transposed

solver.py
@@ -162,27 +162,32 @@ class Solver(object):
 
         # Content loss
         if self.content_loss == 'mse':
-            loss_content = mse(imgs_hr, gen_hr)
+            WH = imgs_hr.shape[2]*imgs_hr.shape[3]
+            
+            loss_content = mse(imgs_hr, gen_hr)/WH
         elif self.content_loss == 'vgg':
             gen_features = self.VGG19(gen_hr)
             real_features = self.VGG19(imgs_hr)
-            loss_content = 0.006 * mse(gen_features, real_features.detach())/real_features.shape[1]**2
+
+            WH = real_features.shape[2]*real_features.shape[3]
+            loss_content = 0.006*mse(gen_features, real_features.detach())/WH
 
         # Adversarial loss
         loss_adversarial = bce(self.D(gen_hr), fake)
         
         # Reconstruction loss
-        loss_reconstruction=l1(self.ds(imgs_hr),self.ds(gen_hr))
+        loss_reconstruction = l1(self.ds(imgs_hr),self.ds(gen_hr))
         """
         TODO
             lambda_rec should be hyperparameter and we can try to tune it.
         """
-        lambda_rec=1e-1
+        #lambda_rec=1e-1
+        lambda_rec=1
 
         # Perceptual loss
-        g_loss = loss_content + 1e-3 * loss_adversarial + lambda_rec * loss_reconstruction
+        g_loss = loss_content + loss_adversarial + lambda_rec * loss_reconstruction
 
-        return g_loss, loss_content, loss_adversarial,loss_reconstruction
+        return g_loss, loss_content, loss_adversarial, loss_reconstruction
 
     def discriminator_loss(self, imgs_lr, imgs_hr, valid, fake):
         bce = nn.BCEWithLogitsLoss().to(self.device)
@@ -318,7 +323,7 @@ class Solver(object):
                 gen_hr = self.G(imgs_lr).to(self.device)
 
                 # Backward and optimize.
-                g_loss, loss_content, loss_adversarial,loss_reconstruction = self.generator_loss(gen_hr, imgs_hr, valid, fake)
+                g_loss, loss_content, loss_adversarial, loss_reconstruction = self.generator_loss(gen_hr, imgs_hr, valid, fake)
                 g_loss.backward()
                 self.g_optimizer.step()
 
@@ -326,6 +331,7 @@ class Solver(object):
                 loss['G/loss'] = g_loss.item()
                 loss['G/loss_content'] = loss_content.item()
                 loss['G/loss_adversarial'] = loss_adversarial.item()
+                loss['G/loss_reconstruction'] = loss_reconstruction.item()
 
                 G_losses.append(g_loss)
                 D_losses.append(d_loss)
@@ -408,13 +414,13 @@ class Solver(object):
         array = np.transpose(np.array([MSE_losses, MSE_seen, MSE_unseen]))
         np.savetxt(os.path.join(self.log_dir, 'losses_MSE.csv'), array, delimiter = ",", header="train, seen, unseen")
         
-        array = np.transpose(np.array([L1_losses, L1_seen, L1_unseen]).transpose())
+        array = np.transpose(np.array([L1_losses, L1_seen, L1_unseen]))
         np.savetxt(os.path.join(self.log_dir, 'losses_L1.csv'), array, delimiter = ",", header="train, seen, unseen")
         
-        array = np.transpose(np.array([PSNR_losses, PSNR_seen, PSNR_unseen]).transpose())
+        array = np.transpose(np.array([PSNR_losses, PSNR_seen, PSNR_unseen]))
         np.savetxt(os.path.join(self.log_dir, 'losses_PSNR.csv'), array, delimiter = ",", header="train, seen, unseen")
         
-        array = np.transpose(np.array([SSIM_losses, SSIM_seen, SSIM_unseen]).transpose())
+        array = np.transpose(np.array([SSIM_losses, SSIM_seen, SSIM_unseen]))
         np.savetxt(os.path.join(self.log_dir, 'losses_SSIM.csv'), array, delimiter = ",", header="train, seen, unseen")
 
         # find losses for bicubic

train_recon_loss.py
@@ -9,7 +9,8 @@ from torch.backends import cudnn
 project_dir = os.path.dirname(os.path.abspath(__file__))
 
 #EXPERIMENTS_DIRECTORY = '/projectnb/dl523/projects/SRGAN/srgan_hw4/experiments/'
-EXPERIMENTS_DIRECTORY = os.path.join( project_dir, 'checkpoints', datetime.now().strftime("%d%b%y_%H%M%S") ) + '/'
+#EXPERIMENTS_DIRECTORY = os.path.join( project_dir, 'checkpoints', datetime.now().strftime("%d%b%y_%H%M%S") ) + '/'
+EXPERIMENTS_DIRECTORY = os.path.join( project_dir, 'checkpoints/24Apr22_112456/' )
 
 def str2bool(v):
     return v.lower() in ('true')
@@ -36,7 +37,7 @@ def main(config):
 def get_experiment_configuration(num_iters=200000,
           log_step=100, sample_step=100, model_save_step=10000, 
           lr_update_step=100, batch_size=16, mode='train', content_loss='mse', 
-          resume_iters=False, load_iters = 300, vgg_layer = 29):
+          resume_iters=False, load_iters = 300, vgg_layer = 29, g_lr=0.0001, d_lr=0.0001):
     config = {}
 
     config['data_dir'] = 'ORL-DATABASE'
@@ -49,11 +50,11 @@ def get_experiment_configuration(num_iters=200000,
     config['batch_size'] = batch_size #16
     config['num_iters'] = num_iters
     config['num_iters_decay'] = num_iters//2
-    config['g_lr'] = 0.0001
-    config['d_lr'] = 0.0001
-    if content_loss == 'vgg':
-        config['g_lr'] = 0.0001
-        config['d_lr'] = 0.0001
+    config['g_lr'] = g_lr
+    config['d_lr'] = d_lr
+    #if content_loss == 'vgg':
+    #    config['g_lr'] = 0.0001
+    #    config['d_lr'] = 0.0001
     config['n_critic'] = 5
     config['beta1'] = 0.5 #v
     config['beta2'] = 0.999 #v
@@ -108,40 +109,42 @@ if __name__ == '__main__':
     cudnn.benchmark = True
     
     #run with mse for first 100
-    config = get_experiment_configuration(num_iters=500, 
-          log_step=10, sample_step=10, model_save_step=10, 
-          batch_size=8, mode='train', content_loss='mse',
-          resume_iters=False, load_iters = 50)
-    main(config)
+    #mse_iters = 1000
+    #config = get_experiment_configuration(num_iters=mse_iters, 
+    #      log_step=10, sample_step=10, model_save_step=10, 
+    #      batch_size=8, mode='train', content_loss='mse',
+    #      resume_iters=False, load_iters = 50)
+    #main(config)
   
-    config = get_experiment_configuration(num_iters=500, 
-          log_step=50, sample_step=50, model_save_step=50, 
-          batch_size=8, mode='test', content_loss='mse',
-          resume_iters=True, load_iters = 500)
-    main(config)
+    #config = get_experiment_configuration(num_iters=mse_iters, 
+    #      log_step=50, sample_step=50, model_save_step=50, 
+    #      batch_size=8, mode='test', content_loss='mse',
+    #      resume_iters=True, load_iters = 1000)
+    #main(config)
     
-    config = get_experiment_configuration(num_iters=500, 
-          log_step=50, sample_step=50, model_save_step=50, 
-          batch_size=8, mode='valid', content_loss='mse',
-          resume_iters=True, load_iters = 500)
-    main(config)
+    #config = get_experiment_configuration(num_iters=mse_iters, 
+    #      log_step=50, sample_step=50, model_save_step=50, 
+    #      batch_size=8, mode='valid', content_loss='mse',
+    #      resume_iters=True, load_iters = 1000)
+    #main(config)
     
     #run with vgg
-    config = get_experiment_configuration(num_iters=1500, 
+    vgg_iters = mse_iters + 2000
+    config = get_experiment_configuration(num_iters=vgg_iters,
           log_step=50, sample_step=50, model_save_step=50, 
           batch_size=8, mode='train', content_loss='vgg',
-          resume_iters=True, load_iters = 500, vgg_layer = 28)
+          resume_iters=True, load_iters = mse_iters, vgg_layer = 28, g_lr=1e-5, d_lr=1e-5)
     main(config)
       
-    config = get_experiment_configuration(num_iters=1500, 
+    config = get_experiment_configuration(num_iters=vgg_iters, 
           log_step=50, sample_step=50, model_save_step=50, 
           batch_size=8, mode='test', content_loss='vgg',
-          resume_iters=False, load_iters=1500)
+          resume_iters=False, load_iters=mse_iters)
     main(config)
     
-    config = get_experiment_configuration(num_iters=1500, 
+    config = get_experiment_configuration(num_iters=vgg_iters, 
           log_step=50, sample_step=50, model_save_step=50, 
           batch_size=8, mode='valid', content_loss='vgg',
-          resume_iters=False, load_iters = 1500)
+          resume_iters=False, load_iters = mse_iters)
     main(config)
 